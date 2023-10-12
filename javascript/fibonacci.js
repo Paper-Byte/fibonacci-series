82cee48555f1ccb8bd57c1a8c99e0f8e0635e9a0
Noah <noahhughesph@gmail.com>
2023-10-11 21:18:34 -0400

    done

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,21 +1,31 @@
 function fibonacci(num) {
-  // type your code here
+  const array = [0, 1, 1];
+  if (num === 0) {
+    return 0;
+  }
+  if (num <= 2) {
+    return 1;
+  }
+  for (let i = 3; i <= num; i++) {
+    array.push(array[i - 1] + array[i - 2]);
+  }
+  return array.pop();
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 0");
-  console.log("=>", fibonacci(0));
+  console.log('Expecting: 0');
+  console.log('=>', fibonacci(0));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: 1");
-  console.log("=>", fibonacci(2));
+  console.log('Expecting: 1');
+  console.log('=>', fibonacci(2));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: 55");
-  console.log("=>", fibonacci(10));
+  console.log('Expecting: 55');
+  console.log('=>', fibonacci(10));
 }
 
 module.exports = fibonacci;
